7ded7c3e184fc57b1d8c477ce3cad3016a339676
Joice Silva <leonardo.marques.firmino@hotmail.com>
2026-01-19 08:48:42 -0300

    feat: cria classe principal da aplicação Spring Boot

---
+++ src/main/java/br/com/api_imp/gestaoimp/GestaoimpApplication.java
@@ -0,0 +1,13 @@
+package br.com.api_imp.gestaoimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoimpApplication.class, args);
+	}
+
+}

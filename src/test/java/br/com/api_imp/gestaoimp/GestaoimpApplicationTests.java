a521c3b1db49a3fcaf9f5d9bca7535f538900f01
Joice Silva <leonardo.marques.firmino@hotmail.com>
2026-01-19 08:49:22 -0300

    test: adiciona testes iniciais da aplicação

---
+++ src/test/java/br/com/api_imp/gestaoimp/GestaoimpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_imp.gestaoimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
